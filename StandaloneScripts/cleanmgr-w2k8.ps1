597342bfb9baa06e81d219e1bb131771c48e513f
davidlahoz <90243715+davidlahoz@users.noreply.github.com>
2021-11-24 19:11:02 +0100

    Update cleanmgr-w2k8.ps1

--- StandaloneScripts/cleanmgr-w2k8.ps1
+++ StandaloneScripts/cleanmgr-w2k8.ps1
@@ -12,6 +12,11 @@ $System32ENus = $env:SystemRoot+'\'+'system32\en-US'
 
 ##Copy Cleanmgr files to windows folders
 ##cleanmgr.exe copy
-Copy-Item -Path $Winsxs\amd64_microsoft-windows-cleanmgr_31bf3856ad364e35_6.1.7600.16385_none_c9392808773cd7da\cleanmgr.exe -Destination $System32
+Copy-Item -Path $Winsxs\amd64_microsoft-windows-cleanmgr_31bf3856ad364e35_6.1.7600.16385_none_c9392808773cd7da\cleanmgr.exe -Destination $System32 '1'; if (-not $?) { Write-Host "UNABLE TO COPY CLEANMGR.EXE TO SYSTEM32" '2'}
+
 ##cleanmgr.exe.mui copy
-Copy-Item -Path $Winsxs\amd64_microsoft-windows-cleanmgr.resources_31bf3856ad364e35_6.1.7600.16385_en-us_b9cb6194b257cc63\cleanmgr.exe.mui -Destination $System32Enus
+Copy-Item -Path $Winsxs\amd64_microsoft-windows-cleanmgr.resources_31bf3856ad364e35_6.1.7600.16385_en-us_b9cb6194b257cc63\cleanmgr.exe.mui -Destination $System32Enus '1'; if (-not $?) { Write-Host "UNABLE TO COPY CLEANMGR.EXE.MUI TO SYSTEM32" '2'}
+
+##Invoke cleanmgr
+Set-Location $System32;cleanmgr.exe
+Exit-PSSession
