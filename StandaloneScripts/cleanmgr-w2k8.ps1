9e46e1e78034da92202450d5a3d6f0167cb6c13d
davidlahoz <90243715+davidlahoz@users.noreply.github.com>
2021-11-24 18:38:58 +0100

    Create cleanmgr-w2k8.ps1
    Copy cleanmgr files to make it work. W2k8 only. UNTESTED

---
+++ StandaloneScripts/cleanmgr-w2k8.ps1
@@ -0,0 +1,17 @@
+﻿##UNTESTED!!!!!!!!!!
+
+
+
+
+## Variables for %SYSTEMROOT%
+## Origins
+$Winsxs = $env:SystemRoot+'\'+'winsxs'
+##Destinations
+$System32 = $env:SystemRoot+'\'+'system32'
+$System32ENus = $env:SystemRoot+'\'+'system32\en-US'
+
+##Copy Cleanmgr files to windows folders
+##cleanmgr.exe copy
+Copy-Item -Path $Winsxs\amd64_microsoft-windows-cleanmgr_31bf3856ad364e35_6.1.7600.16385_none_c9392808773cd7da\cleanmgr.exe -Destination $System32
+##cleanmgr.exe.mui copy
+Copy-Item -Path $Winsxs\amd64_microsoft-windows-cleanmgr.resources_31bf3856ad364e35_6.1.7600.16385_en-us_b9cb6194b257cc63\cleanmgr.exe.mui -Destination $System32Enus
