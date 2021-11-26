01f5d3c3f179ea7d9ef1d29e0fce017e5acb5c3a
davidlahoz <90243715+davidlahoz@users.noreply.github.com>
2021-11-26 17:15:22 +0100

    Fixed issues stablishing new variables

--- StandaloneScripts/cleanmgr-w2k8.ps1
+++ StandaloneScripts/cleanmgr-w2k8.ps1
@@ -1,23 +1,19 @@
-﻿Write-Host "SCRIPT UNTESTED! PRESS ANY KEY TO CONTINUE" -ForegroundColor Red -BackgroundColor DarkRed
-Pause
-
-
-
-
-## Variables for %SYSTEMROOT%
-## Origins
+﻿## Path definition for script. || Automatically selects %systemroot%/winsxs despite which local drive is using
 $Winsxs = $env:SystemRoot+'\'+'winsxs'
 ##Destinations
 $System32 = $env:SystemRoot+'\'+'system32'
-$System32ENus = $env:SystemRoot+'\'+'system32\en-US'
+$System32EN_US = $env:SystemRoot+'\'+'system32\en-US'
+
+#CLEANMGR assets
+$CleanmgrEXE = $Winsxs + "\amd64_microsoft-windows-cleanmgr_31bf3856ad364e35_6.1.7600.16385_none_c9392808773cd7da\cleanmgr.exe"
+$CleanmgrEXEMUI = $Winsxs + "\amd64_microsoft-windows-cleanmgr.resources_31bf3856ad364e35_6.1.7600.16385_en-us_b9cb6194b257cc63\cleanmgr.exe.mui"
 
 ##Copy Cleanmgr files to windows folders
 ##cleanmgr.exe copy
-Copy-Item -Path "$Winsxs\amd64_microsoft-windows-cleanmgr_31bf3856ad364e35_6.1.7600.16385_none_c9392808773cd7da\cleanmgr.exe" -Destination "$System32" '1'; if (-not $?) { Write-Host "UNABLE TO COPY CLEANMGR.EXE TO SYSTEM32" -ForegroundColor Red '2'}
+Copy-Item -Path $CleanmgrEXE -Destination "$System32"
 
 ##cleanmgr.exe.mui copy
-Copy-Item -Path "$Winsxs\amd64_microsoft-windows-cleanmgr.resources_31bf3856ad364e35_6.1.7600.16385_en-us_b9cb6194b257cc63\cleanmgr.exe.mui" -Destination "$System32Enus" '1'; if (-not $?) { Write-Host "UNABLE TO COPY CLEANMGR.EXE.MUI TO SYSTEM32" -ForegroundColor Red '2'}
+Copy-Item -Path $CleanmgrEXEMUI -Destination "$System32EN_US"
 
-##Invoke cleanmgr
-Set-Location -Path $System32;cleanmgr.exe
-Exit-PSSession
+####### UNCOMMENT NEXT LINE IF IT'S BEING USED ON LOCAL AND WANT TO LAUNCH CLEANGR AFTER COPING #########
+#Set-Location -Path $System32;cleanmgr.exe
