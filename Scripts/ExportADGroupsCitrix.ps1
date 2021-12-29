33857ea814263ff942382cf636aea586b7c0be81
davidlahoz <90243715+davidlahoz@users.noreply.github.com>
2021-12-29 16:44:37 +0100

    ExportAdGroupsCitrix export to HTML
    Instead CSV, now the script exports to HTML with simple CSS to be more user friendly (in order to have it hosted as a website.
    Also includes a (commented) replace line, if needed to hide any desired group (instead a 'for each' which might be even longer to code).

--- Scripts/ExportADGroupsCitrix.ps1
+++ Scripts/ExportADGroupsCitrix.ps1
@@ -1,11 +1,36 @@
-#Setting working directory
 New-Item -ItemType Directory -Force -Path C:\TEMP
 Set-Location -Path C:\temp
 #Listing Citrix Apps installed. Returns name and AD Groups/users that have Visibility for the app
-Get-BrokerApplication -MaxRecordCount 90000 | Select-Object PublishedName,@{Name=’AssociatedUserFullNames’;Expression={[string]::join(“;”, ($_.AssociatedUserFullNames))}}
-#Export to CSV
-Export-CSV -Path .\CitrixApps.csv
-#end message
-$Message = " CSV CitrixApps.csv generated on C:\temp!`n"
-Write-Host $Message -ForegroundColor Green
-Read-Host -Prompt "Press any key to continue"
+Get-BrokerApplication -MaxRecordCount 90000 | Select-Object PublishedName,@{Name=’AssociatedUserFullNames’;Expression={[string]::join(“;”, ($_.AssociatedUserFullNames))}} | ConvertTo-Html -Title "Citrix Applications - AD Groups" | Out-File .\index.html
+
+#replaces strings and adds CSS link
+(Get-Content c:\temp\index.html).replace('PublishedName', 'Citrix App Name') | Set-Content c:\temp\index.html
+(Get-Content c:\temp\index.html).replace('AssociatedUserFullNames', 'ActiveDirectory Group') | Set-Content c:\temp\index.html
+(Get-Content c:\temp\index.html).replace('</title>', '</title> <link rel="stylesheet" href="style.css">') | Set-Content c:\temp\index.html
+(Get-Content c:\temp\index.html).replace('<body>', '<body> <h1>CITRIX APPS AND ITS AD GROUPS</h1><p><strong>If "ActiveDirectory Group" is blank/empty or only has users instead of AD Groups, please submit a ticket to BASE PLATFORMS/DISTRIBUTED SERVERS</strong></p>') | Set-Content c:\temp\index.html
+#Citrix related ADGroups deletion from table (REPLACE DUMMY-ADGROUP AND UNCOMMENT IF NEEDED )
+#(Get-Content c:\temp\index.html).replace('DUMMY-ADGROUP', '') | Set-Content c:\temp\index.html
+
+#CSS file creation
+New-Item -Path C:\temp -Name "style.css" -Force -ItemType "file" -Value "table {
+    border: none;
+    width: 100%;
+    border-collapse: collapse;
+}
+
+td { 
+    padding: 5px 10px;
+    text-align: center;
+    border: 1px solid #999;
+}
+
+tr:nth-child(1) {
+    background: #0089fa;
+    color: white;
+}
+h1 {
+    text-align: center;
+}
+strong{
+    color: red;
+}"
